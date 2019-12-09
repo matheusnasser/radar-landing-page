0359bbe16f1218d28901eb61629fac4797436133
Naasser <matheusnasser12@gmail.com>
2019-12-09 18:06:25 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('ul.nav a').bind('click',function(event){
+        var $anchor = $(this);
+
+        $('html, body').stop().animate({
+            scrollLeft: $($anchor.attr('href')).offset().left
+        }, 1000);
+        event.preventDefault();
+    });
+});
