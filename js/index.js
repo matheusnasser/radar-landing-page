463cd303034a05738891fee9c944a51b9a4b8d8a
Matheus Nasser <33842815+matheusnasser@users.noreply.github.com>
2020-01-15 00:42:35 -0300

    ''modal feito"

--- js/index.js
+++ js/index.js
@@ -9,22 +9,15 @@ $(function() {
 	});
   });
 
-  var modal = document.getElementById("myModal");
+  
 
-  var btn = document.getElementById("myBtn");
-  
-  var span = document.getElementsByClassName("close")[0];
-  
-  btn.onclick = function() {
-	modal.style.display = "block";
-  }
-  
-  span.onclick = function() {
-	modal.style.display = "none";
-  }
-  
-  window.onclick = function(event) {
-	if (event.target == modal) {
-	  modal.style.display = "none";
-	}
-  }
+  $(document).ready(function(){
+	$("#btn-produtor").click(function(){
+	  $("#produtor").fadeIn(1500,() => {
+		  $("#produtor-txt").fadeIn(500).removeClass("hidden");
+	  });
+
+	});
+  });
+
+
